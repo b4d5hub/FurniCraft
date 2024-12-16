6f9363e210b07e9686df63b897457ace33632983
sarraadaoudi <sara.daoudi.03@edu.uiz.ac.ma>
2024-12-16 17:48:06 +0100

    Amélioration de l'expérience utilisateur, ajout du tri des produits et de la page 'À propos' au menu principal.

---
+++ wp-content/languages/loco/themes/sober-fr_FR.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'Sober Child Theme','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-12-16 15:52+0000','po-revision-date'=>'2024-12-16 15:53+0000','last-translator'=>'','language-team'=>'Français','language'=>'fr_FR','plural-forms'=>'nplurals=2; plural=n > 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.14; wp-6.7.1','x-domain'=>'sober','messages'=>[]];
